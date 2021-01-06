d89c2b9ff39fe353edd15a82bf5cd1f57cba5242
Pascal Betting <pascal.betting@gmx.de>
2021-01-06 12:53:53 +0100

    dev: improve dev workflow by not creating new log files
    -> no need to restart less

--- watch-and-deploy.sh
+++ watch-and-deploy.sh
@@ -6,7 +6,7 @@ ln -sf $(pwd) ~/.local/share/ulauncher/extensions/
 pkill ulauncher
 ulauncher --no-extensions --dev -v > /tmp/ulauncher.log 2>&1 &
 
-sleep 1
+sleep 2
 
 # redeploy app when files change
 export VERBOSE=1 
@@ -14,7 +14,7 @@ export ULAUNCHER_WS_API=ws://127.0.0.1:5054/ulauncher-habitify
 export PYTHONPATH=/usr/lib/python3/dist-packages 
 
 startExtension() {
-    /usr/bin/python3 $HOME/.local/share/ulauncher/extensions/ulauncher-habitify/main.py > /tmp/ulauncher-extension.log 2>&1 &
+    /usr/bin/python3 $HOME/.local/share/ulauncher/extensions/ulauncher-habitify/main.py >> /tmp/ulauncher-extension.log 2>&1 &
     pid=$(echo $!)
 }
 
